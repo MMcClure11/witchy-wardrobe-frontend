d105354aab6650a62eca6ca4524759edf29a906a
MMcClure11 <mcclme02@luther.edu>
2020-09-24 11:49:09 -0500

    adds createCard and cardContent functions in Item class, item Names and Images successfully rendering to DOM

--- src/Item.js
+++ src/Item.js
@@ -1,9 +1,26 @@
 class Item {
   constructor(item){
     this.item = item
-    console.log(this)
+    this.card = this.createCard()
   }
 
-  
+  createCard(){
+    const card = document.createElement('div')
+    card.className = "card"
+    card.dataset.id = this.item.id
+    this.cardContent(card)
+    app.appendChild(card)
+  }
+
+  cardContent(card) {
+    const {name, image, color, date_purchased, store, manufacture_location, cost, times_used} = this.item
+    const itemName = document.createElement('h2')
+    itemName.className = 'h2'
+    itemName.innerText = name
+    const itemImg = document.createElement('img')
+    itemImg.src = image
+
+    card.append(itemName, itemImg)
+  }
 
 }
