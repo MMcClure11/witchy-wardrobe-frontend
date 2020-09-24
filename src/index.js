b3030f9bc5b15352bbacf9d95ea6247eb37aff40
MMcClure11 <mcclme02@luther.edu>
2020-09-24 11:16:11 -0500

    adds createAppDiv cuntion and displayItems functions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 console.log("hello from index.js")
 const BASE_URL = 'http://localhost:3000';
 const ITEMS_URL = `${BASE_URL}/items`;
-const body = document.querySelector('body')
-const app = document.createElement('div')
-const itemsBtn = document.querySelector("#items-btn")
-console.log(itemsBtn)
+const body = document.querySelector('body');
+const app = document.createElement('div');
+const itemsBtn = document.querySelector("#items-btn");
+
+createAppDiv();
+displayItems();
+
+function createAppDiv() {
+  app.setAttribute('id', 'app-div')
+  body.appendChild(app)
+}
+
+function displayItems(){
+  itemsBtn.addEventListener("click", () => {
+    alert("items button")
+    app.innerHTML = ""
+  })
+}
 
 
 
-fetch(`${BASE_URL}/items`)
-  .then(res => res.json())
-  .then( items => console.log(items))
+// fetch(`${BASE_URL}/items`)
+//   .then(res => res.json())
+//   .then( items => console.log(items))
 
