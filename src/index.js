b55b7ce26b818d79779950f76dbf3ed00d7c0ba2
MMcClure11 <mcclme02@luther.edu>
2020-09-24 13:54:10 -0500

    adds js to remove modal on click

--- src/index.js
+++ src/index.js
@@ -43,3 +43,20 @@ function initItems(){
     })
   })
 }
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+  modal.querySelector("form").remove()
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    modal.querySelector("form").remove()
+  }
+}
