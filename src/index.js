a5ff71b44dbaf68dced4809f90db9603d71373d2
MMcClure11 <mcclme02@luther.edu>
2020-09-24 12:13:15 -0500

    makes outfits button functional, clears out innerHTMl for future use

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ const ITEMS_URL = `${BASE_URL}/items`;
 const body = document.querySelector('body');
 const app = document.createElement('div');
 const itemsBtn = document.querySelector("#items-btn");
+const outfitsBtn = document.querySelector("#outfits-btn");
 
 createAppDiv();
 displayItems();
+displayOutfits();
 
 function createAppDiv() {
   app.setAttribute('id', 'app-div')
@@ -17,6 +19,15 @@ function displayItems(){
   itemsBtn.addEventListener("click", () => {
     app.innerHTML = "";
     initItems();
+  })
+}
+
+function displayOutfits(){
+  outfitsBtn.addEventListener("click", () => {
+    app.innerHTML = "";
+    const h1 = document.createElement('h1')
+    h1.innerText = "Coming Soon."
+    app.appendChild(h1);
   })
 }
 
