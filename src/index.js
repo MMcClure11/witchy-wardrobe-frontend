5f98a1656daa9576c83b78c0f3620d37878bcf34
MMcClure11 <mcclme02@luther.edu>
2020-09-24 11:10:17 -0500

    adds nav with buttons for clothing and outfits, adds const for items_url, body, an app div and itemsBtn to index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
+console.log("hello from index.js")
 const BASE_URL = 'http://localhost:3000';
+const ITEMS_URL = `${BASE_URL}/items`;
+const body = document.querySelector('body')
+const app = document.createElement('div')
+const itemsBtn = document.querySelector("#items-btn")
+console.log(itemsBtn)
+
+
 
 fetch(`${BASE_URL}/items`)
   .then(res => res.json())
