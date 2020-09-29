652dc2a83c1f23dc40a3f2d0a13721525f97bdd3
MMcClure11 <mcclme02@luther.edu>
2020-09-29 14:13:44 -0500

    adds ItemForm class and moves editHandler and editFormContent to ItemForm Class

--- src/Item.js
+++ src/Item.js
@@ -68,7 +68,7 @@ class Item {
     editBtn.id = 'edit-btn'
     editBtn.innerText = `Edit`
     const editItemForm = document.createElement('form')
-    this.itemEditHandler(editBtn, editItemForm, name, image, color, date_purchased, store, manufacture_location, cost, times_used)
+    ItemForm.itemEditHandler(editBtn, editItemForm, name, image, color, date_purchased, store, manufacture_location, cost, times_used)
     editItemForm.addEventListener("submit", (e) => {
       e.preventDefault();
       // console.log(this.item.id)
@@ -102,126 +102,6 @@ class Item {
     deleteBtn.addEventListener("click", () => {
       ApiService.deleteItem(this.item.id).then(() => card.remove())
     })
-  }
-
-  itemEditHandler(editBtn, editItemForm, name, image, color, date_purchased, store, manufacture_location, cost, times_used){
-    editBtn.addEventListener("click", () => {
-      // console.log(this.item)
-      modal.style.display = "block"
-      modalContent.append(editItemForm)
-      this.renderEditFormContent(editItemForm, name, image, color, date_purchased, store, manufacture_location, cost, times_used)
-    })
-  }
-
-  renderEditFormContent(editItemForm, name, image, color, date_purchased, store, manufacture_location, cost, times_used){
-    editItemForm.innerHTML = ""
-    const itemNameDiv = document.createElement('div')
-    itemNameDiv.className = 'form-group'
-    const itemNameLabel = document.createElement('label')
-    itemNameLabel.innerText = "Item Name:"
-    const itemNameInput = document.createElement('input')
-    itemNameInput.name = "name"
-    itemNameInput.value = name
-    itemNameInput.className = "form-control"
-    itemNameDiv.append(itemNameLabel, itemNameInput)
-
-    const itemImageDiv = document.createElement('div')
-    itemImageDiv.className = 'form-group'
-    const itemImageLabel = document.createElement('label')
-    itemImageLabel.innerText = "Item Image URL:"
-    const itemImageInput = document.createElement('input')
-    itemImageInput.name = "image"
-    itemImageInput.value = image
-    itemImageInput.className = "form-control"
-    itemImageDiv.append(itemImageLabel, itemImageInput)
-
-    const itemColorDiv = document.createElement('div')
-    itemColorDiv.className = 'form-group'
-    const itemColorLabel = document.createElement('label')
-    itemColorLabel.innerText = "Item Color:"
-    const itemColorInput = document.createElement('input')
-    itemColorInput.name = "color"
-    itemColorInput.value = color
-    itemColorInput.className = "form-control"
-    itemColorDiv.append(itemColorLabel, itemColorInput)
-
-    const itemDatePurchasedDiv = document.createElement('div')
-    itemDatePurchasedDiv.className = 'form-group'
-    const itemDatePurchasedLabel = document.createElement('label')
-    itemDatePurchasedLabel.innerText = "Date Purchased:"
-    const itemDatePurchasedInput = document.createElement('input')
-    itemDatePurchasedInput.name = "date_purchased"
-    itemDatePurchasedInput.type = "date"
-    itemDatePurchasedInput.value = date_purchased
-    itemDatePurchasedInput.className = "form-control"
-    itemDatePurchasedDiv.append(itemDatePurchasedLabel, itemDatePurchasedInput)
-
-    const itemStoreDiv = document.createElement('div')
-    itemStoreDiv.className = 'form-group'
-    const itemStoreLabel = document.createElement('label')
-    itemStoreLabel.innerText = "Store Purchased From:"
-    const itemStoreInput = document.createElement('input')
-    itemStoreInput.name = "store"
-    itemStoreInput.value = store
-    itemStoreInput.className = "form-control"
-    itemStoreDiv.append(itemStoreLabel, itemStoreInput)
-
-    const itemManuLocDiv = document.createElement('div')
-    itemManuLocDiv.className = 'form-group'
-    const itemManuLocLabel = document.createElement('label')
-    itemManuLocLabel.innerText = "Made In:"
-    const itemManuLocInput = document.createElement('input')
-    itemManuLocInput.name = "manufacture_location"
-    itemManuLocInput.value = manufacture_location
-    itemManuLocInput.className = "form-control"
-    itemManuLocDiv.append(itemManuLocLabel, itemManuLocInput)
-
-    const itemCostDiv = document.createElement('div')
-    itemCostDiv.className = 'form-group'
-    const itemCostLabel = document.createElement('label')
-    itemCostLabel.innerText = "Cost:"
-    const itemCostInput = document.createElement('input')
-    itemCostInput.type = "number"
-    itemCostInput.step = "0.01"
-    itemCostInput.name = "cost"
-    itemCostInput.value = cost
-    itemCostInput.className = "form-control"
-    itemCostDiv.append(itemCostLabel, itemCostInput)
-
-    const itemTimesUsedDiv = document.createElement('div')
-    itemTimesUsedDiv.className = 'form-group'
-    const itemTimesUsedLabel = document.createElement('label')
-    itemTimesUsedLabel.innerText = "Times Used:"
-    const itemTimesUsedInput = document.createElement('input')
-    itemTimesUsedInput.type = "number"
-    itemTimesUsedInput.name = "times_used"
-    itemTimesUsedInput.value = times_used
-    itemTimesUsedInput.className = "form-control"
-    itemTimesUsedDiv.append(itemTimesUsedLabel, itemTimesUsedInput)
-
-    const itemCatDiv = document.createElement('div')
-    itemCatDiv.className = 'form-group'
-    const categorySelectorLabel = document.createElement('label')
-    categorySelectorLabel.innerText = "Category:"
-    const categorySelector = document.createElement('select')
-    categorySelector.id = 'select-category'
-    categorySelector.name = 'category'
-    categorySelector.className = 'form-control'
-    Item.categoryDropdown(categorySelector)
-
-    const submitBtn = document.createElement('button')
-    submitBtn.className = 'btn'
-    submitBtn.innerText = "Submit"
-
-    editItemForm.append(itemNameDiv,
-      itemImageDiv, 
-      itemColorDiv, 
-      itemDatePurchasedDiv, 
-      itemStoreDiv,
-      itemManuLocDiv,
-      itemCostDiv, 
-      itemTimesUsedDiv, 
-      categorySelectorLabel, categorySelector, submitBtn)
   }
 
   updateItemHandler(editedItem, card){
