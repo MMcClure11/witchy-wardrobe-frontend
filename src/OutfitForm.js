268fb65b9b3d8ca0f82d4da7211b78173e66ddb7
MMcClure11 <mcclme02@luther.edu>
2020-10-01 12:39:16 -0500

    changes outfitForm class to use one renderOutfitForm and adds conditionals to check for edit values, removes outfitForm function for post

--- src/OutfitForm.js
+++ src/OutfitForm.js
@@ -11,52 +11,8 @@ class OutfitForm {
     const outfitForm = document.createElement('form')
     modalContent.innerHTML = ""
     modalContent.append(outfitForm)
-    OutfitForm.outfitFormContent(outfitForm)
+    OutfitForm.renderFormContent(outfitForm)
     outfitForm.addEventListener('submit', OutfitForm.handleFormSubmit)
-  }
-
-  static outfitFormContent(outfitForm){
-    const outfitNameDiv = document.createElement('div')
-    outfitNameDiv.className = 'form-group'
-    const outfitNameLabel = document.createElement('label')
-    outfitNameLabel.innerText = "Outfit Name:"
-    const outfitNameInput = document.createElement('input')
-    outfitNameInput.name = "name"
-    outfitNameInput.required = true
-    outfitNameInput.className = "form-control"
-    outfitNameDiv.append(outfitNameLabel, outfitNameInput)
-
-    const outfitLikesDiv = document.createElement('div')
-    outfitLikesDiv.className = 'form-group'
-    const outfitLikesLabel = document.createElement('label')
-    outfitLikesLabel.innerText = "Love for this Outfit:"
-    const outfitLikesInput = document.createElement('input')
-    outfitLikesInput.name = "likes"
-    outfitLikesInput.required = true
-    outfitLikesInput.type = "number" 
-    outfitLikesInput.className = "form-control"
-    outfitLikesDiv.append(outfitLikesLabel, outfitLikesInput)
-
-    const itemsCheckContainer = document.createElement('div')
-    itemsCheckContainer.className = "items-check-container"
-    const itemsCheck = document.createElement('div')
-    const checkboxLabel = document.createElement('label')
-    checkboxLabel.innerText = "Pick your clothes for your Outfit:"
-    itemsCheckContainer.append(checkboxLabel, itemsCheck)
-
-    ApiService.getAllItems()
-      .then(items => {
-        items.forEach(item => {
-          itemsCheck.innerHTML += `<input class="checks" type="checkbox" id="${item.id}" name="${item.name}">
-          <label>${item.name}</label>`
-        })
-      })
-
-    const submitBtn = document.createElement('button')
-    submitBtn.className = 'btn'
-    submitBtn.innerText = "Submit"
-
-    outfitForm.append(outfitNameDiv, outfitLikesDiv, itemsCheckContainer, submitBtn)
   }
 
   static handleFormSubmit(e) {
@@ -88,19 +44,18 @@ class OutfitForm {
     editBtn.addEventListener("click", () => {
       modal.style.display = "block"
       modalContent.append(editOutfitForm)
-      OutfitForm.renderEditFormContent(editOutfitForm, name, likes, items)
+      OutfitForm.renderFormContent(editOutfitForm, name, likes, items)
     })
   }
 
-  static renderEditFormContent(editOutfitForm, name, likes, selectedItems){
-    editOutfitForm.innerHTML = ""
+  static renderFormContent(editOutfitForm, name, likes, selectedItems, outfitForm){
     const outfitNameDiv = document.createElement('div')
     outfitNameDiv.className = 'form-group'
     const outfitNameLabel = document.createElement('label')
     outfitNameLabel.innerText = "Outfit Name:"
     const outfitNameInput = document.createElement('input')
     outfitNameInput.name = "name"
-    outfitNameInput.value = name
+    if(name){outfitNameInput.value = name}
     outfitNameInput.required = true
     outfitNameInput.className = "form-control"
     outfitNameDiv.append(outfitNameLabel, outfitNameInput)
@@ -111,7 +66,7 @@ class OutfitForm {
     outfitLikesLabel.innerText = "Love for this Outfit:"
     const outfitLikesInput = document.createElement('input')
     outfitLikesInput.name = "likes"
-    outfitLikesInput.value = likes
+    if(likes){outfitLikesInput.value = likes }
     outfitLikesInput.required = true
     outfitLikesInput.type = "number" 
     outfitLikesInput.className = "form-control"
@@ -127,8 +82,6 @@ class OutfitForm {
     ApiService.getAllItems(selectedItems)
       .then(items => {
         items.forEach(item => {
-          // itemsCheck.innerHTML += `<input class="checks" type="checkbox" id="${item.id}" name="${item.name}">
-          // <label>${item.name}</label>`
           let checkbox = document.createElement('input')
           checkbox.className = "checks"
           checkbox.type = "checkbox"
@@ -136,11 +89,13 @@ class OutfitForm {
           checkbox.name = item.name
           let checkLabel = document.createElement('label')
           checkLabel.innerText = item.name
-          selectedItems.forEach( item => {
-            if(item.name === checkbox.name){
-              checkbox.checked = true
-            }
-          })
+          if(selectedItems){
+            selectedItems.forEach( item => {
+              if(item.name === checkbox.name){
+                checkbox.checked = true
+              }
+            })
+          }
           itemsCheck.append(checkbox, checkLabel)
         })
       })
@@ -149,7 +104,13 @@ class OutfitForm {
     submitBtn.className = 'btn'
     submitBtn.innerText = "Submit"
 
-    editOutfitForm.append(outfitNameDiv, outfitLikesDiv, itemsCheckContainer, submitBtn)
+    if(editOutfitForm){
+      editOutfitForm.innerHTML = ""
+      editOutfitForm.append(outfitNameDiv, outfitLikesDiv, itemsCheckContainer, submitBtn)
+    } else if (outfitForm) {
+      outfitForm.innerHTML = ""
+      outfitForm.append(outfitNameDiv, outfitLikesDiv, itemsCheckContainer, submitBtn)
+    }
   }
 
 }
