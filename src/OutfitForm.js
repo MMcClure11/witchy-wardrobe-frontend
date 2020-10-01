ff4f08440c90ba8781adab6bcb0579673c9e9298
MMcClure11 <mcclme02@luther.edu>
2020-10-01 08:57:07 -0500

    appends attribute input fields to editOutfitForm

--- src/OutfitForm.js
+++ src/OutfitForm.js
@@ -92,7 +92,50 @@ class OutfitForm {
   }
 
   static renderEditFormContent(editOutfitForm, name, likes, items){
-    
+    editOutfitForm.innerHTML = ""
+    const outfitNameDiv = document.createElement('div')
+    outfitNameDiv.className = 'form-group'
+    const outfitNameLabel = document.createElement('label')
+    outfitNameLabel.innerText = "Outfit Name:"
+    const outfitNameInput = document.createElement('input')
+    outfitNameInput.name = "name"
+    outfitNameInput.value = name
+    outfitNameInput.required = true
+    outfitNameInput.className = "form-control"
+    outfitNameDiv.append(outfitNameLabel, outfitNameInput)
+
+    const outfitLikesDiv = document.createElement('div')
+    outfitLikesDiv.className = 'form-group'
+    const outfitLikesLabel = document.createElement('label')
+    outfitLikesLabel.innerText = "Love for this Outfit:"
+    const outfitLikesInput = document.createElement('input')
+    outfitLikesInput.name = "likes"
+    outfitLikesInput.value = likes
+    outfitLikesInput.required = true
+    outfitLikesInput.type = "number" 
+    outfitLikesInput.className = "form-control"
+    outfitLikesDiv.append(outfitLikesLabel, outfitLikesInput)
+
+    const itemsCheckContainer = document.createElement('div')
+    itemsCheckContainer.className = "items-check-container"
+    const itemsCheck = document.createElement('div')
+    const checkboxLabel = document.createElement('label')
+    checkboxLabel.innerText = "Pick your clothes for your Outfit:"
+    itemsCheckContainer.append(checkboxLabel, itemsCheck)
+
+    ApiService.getAllItems()
+      .then(items => {
+        items.forEach(item => {
+          itemsCheck.innerHTML += `<input class="checks" type="checkbox" id="${item.id}" name="${item.name}">
+          <label>${item.name}</label>`
+        })
+      })
+
+    const submitBtn = document.createElement('button')
+    submitBtn.className = 'btn'
+    submitBtn.innerText = "Submit"
+
+    editOutfitForm.append(outfitNameDiv, outfitLikesDiv, itemsCheckContainer, submitBtn)
   }
 
 }
