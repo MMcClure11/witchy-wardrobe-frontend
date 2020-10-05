78be7effd59d28bc9f860bd9589437c2406d7330
MMcClure11 <mcclme02@luther.edu>
2020-10-04 19:54:52 -0500

    adds content for 4x4 capsule and spark joy

--- src/Home.js
+++ src/Home.js
@@ -24,8 +24,11 @@ class Home {
             <a href="#profile" class="nav-link">Project 333</a>
         </li>
         <li class="nav-item">
-            <a href="#messages" class="nav-link">Messages</a>
+            <a href="#messages" class="nav-link">4x4 Capsule</a>
         </li>
+        <li class="nav-item">
+        <a href="#joy" class="nav-link">Joy</a>
+    </li>
     </ul>
     <div class="tab-content">
         <div class="tab-pane fade show active" id="home">
@@ -46,17 +49,31 @@ class Home {
             <p>Check out <a class="links" href="https://bemorewithless.com/">Courtney Carver's website</a> to learn more about how to get started.</p>
             </div>
         <div class="tab-pane fade" id="messages">
-            <h4 class="mt-2">Messages tab content</h4>
-            <p>Donec vel placerat quam, ut euismod risus. Sed a mi suscipit, elementum sem a, hendrerit velit. Donec at erat magna. Sed dignissim orci nec eleifend egestas. Donec eget mi consequat massa vestibulum laoreet. Mauris et ultrices nulla, malesuada volutpat ante. Fusce ut orci lorem. Donec molestie libero in tempus imperdiet. Cum sociis natoque penatibus et magnis.</p>
+            <h4 class="mt-2">Create a Capsule Wardrobe</h4>
+            <p>Try using themed colors throughout your entire wardrobe so you can pair down to just 16 or fewer pieces of clothing. </p>
+            <p>The mix and max nature of this type of clothing setup makes it ideal for creating a minimalist wardrobe or when preparing to travel.</p>
+            <p>Check out the <a class="links" href="https://www.theviviennefiles.com/four-by-four-wardrobes/">Vivienne Files</a> to learn more about the capsule wardrobe and get great ideas for color schemes.</p>    
+        </div>
+        <div class="tab-pane fade" id="profile">
+          <h4 class="mt-2">Joy</h4>
+          <p>The premise: Wear just 33 items for 3 months and find the joy you miss out on when you spend too much time worrying about what to wear.</p>
+          <p>Check out <a class="links" href="https://bemorewithless.com/">Courtney Carver's website</a> to learn more about how to get started.</p>
+        </div>
+        <div class="tab-pane fade" id="joy">
+            <h4 class="mt-2">Spark Joy</h4>
+            <p>Do your clothes bring you joy? </p>
+            <p>You probably have some items that you love and wear very often, and others that rarely see the light of day but you can't seem to get rid of them "just in case".</p>
+            <p>Check out Mari Kondo's <a class="links" href="https://konmari.com/">official website</a> to learn more about her method of tidying up.</p>
+            <p>For a quick look at just doing your closet in this way, have a look at this <a class="links" href="https://makinglemonadeblog.com/10-steps-decluttering-your-closet-clothing-konmari-method/">making lemonade blog</a>.</p>    
         </div>
     </div>`
-$(document).ready(function(){ 
-  $("#info-tab a").click(function(e){
-      e.preventDefault();
-      $(this).tab('show');
+  $(document).ready(function(){ 
+    $("#info-tab a").click(function(e){
+        e.preventDefault();
+        $(this).tab('show');
+    });
   });
-});
-app.appendChild(infoDiv)
+  app.appendChild(infoDiv)
   }
 
 }
